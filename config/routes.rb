903af059085a4deb8a765f9a3ca1a8284b853408
Steven Galvin <steven.john.galvin@gmail.com>
2017-07-25 16:45:48 -0700

    add devise routes. begin styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root to: 'pages#home'
+
+  get 'home' => 'pages#home'
 end
